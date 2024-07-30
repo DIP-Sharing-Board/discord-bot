bc4e729b584ff547f1c5641b7d8800b4dbc7246f
kittipong-dev <achikrab@gmail.com>
2024-07-30 23:04:52 +0700

    change env name db password

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@
 # Read the secret from the file and export it as an environment variable
 export BOT_TOKEN=$(cat /run/secrets/bot-token)
 export CHANNELS_ID=$(cat /run/secrets/channels-id)
-export MYSQL_ROOT_PASSWORD=$(cat /run/secrets/db-password)
+export DATABASE_PASSWORD=$(cat /run/secrets/db-password)
 # Now you can start your main application
 exec python main.py
